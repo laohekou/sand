7eb3763e8932cd48b9c45e4a398ba01308a28f8c
xiongy <15810265860@163.com>
2022-02-09 15:56:48 +0800

    日志

--- src/Contract/AbstractGateway.php
+++ src/Contract/AbstractGateway.php
@@ -193,7 +193,7 @@ abstract class AbstractGateway implements GatewayInterface
             get_logger('SAND-RESP','api-log')->info($resp);
             if($resp) {
                 $result = $this->parseResult($resp);
-                get_logger('SAND-RESP--01','api-log')->info(json_encode($resp,JSON_UNESCAPED_UNICODE));
+                get_logger('SAND-RESP--01','api-log')->info(json_encode($result,JSON_UNESCAPED_UNICODE));
 
                 return $result;
             }
